7b315e6355623300200922d855f7279652f83b5b
devjoshi05 <devmjoshi05@gmail.com>
2025-07-10 19:33:10 +0530

    Update steamlit_app.py
    info

--- steamlit_app.py
+++ steamlit_app.py
@@ -48,3 +48,8 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get ("https://my.smoothiefroot.com/api/fruit/watermelon")
+st. text (smoothiefroot_response.json())
